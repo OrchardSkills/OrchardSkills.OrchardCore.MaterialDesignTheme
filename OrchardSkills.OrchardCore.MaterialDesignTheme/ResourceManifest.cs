d92ef760fcbb24ebe714bb205b9a742d2f7d6f3d
senshi111 <banenicovic@gmail.com>
2020-08-20 16:21:45 +0200

    remove uncompress css and js files

--- OrchardSkills.OrchardCore.MaterialDesignTheme/ResourceManifest.cs
+++ OrchardSkills.OrchardCore.MaterialDesignTheme/ResourceManifest.cs
@@ -12,24 +12,24 @@ namespace OrchardCore.Themes.MaterialDesignTheme
 
             manifest
                 .DefineStyle("MaterialDesignTheme-css" )
-                .SetUrl("~/MaterialDesignTheme/css/mdb.min.css", "~/MaterialDesignTheme/css/mdb.css")
+                .SetUrl("~/MaterialDesignTheme/css/mdb.min.css")
                 .SetVersion("1.0.0");
 
             manifest
                 .DefineScript("MaterialDesignTheme-js")
-                .SetUrl("~/MaterialDesignTheme/js/mdb.min.js", "~/MaterialDesignTheme/js/mdb.js")
+                .SetUrl("~/MaterialDesignTheme/js/mdb.min.js")
                 .SetVersion("1.0.0");
 
             manifest
                 .DefineStyle("MaterialDesignTheme-vendor-font-awesome")
-                .SetUrl("~/MaterialDesignTheme/vendor/fontawesome-free/css/all.min.css", "~/MaterialDesignTheme/vendor/fontawesome-free/css/all.css")
-                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/css/all.min.css", "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/css/all.css")
+                .SetUrl("~/MaterialDesignTheme/vendor/fontawesome-free/css/all.min.css")
+                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/css/all.min.css")
                 .SetCdnIntegrity("sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V", "sha384-I4s88/QBf6OKVFMRRyjRY9wYdRoEO/PnNLQ1xY+G0OQfntKp8FxRsOg9qjmtbnvL")
                 .SetVersion("5.13.0");
 
             manifest
                 .DefineScript("MaterialDesignTheme-vendor-font-awesome")
-                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/js/all.min.js", "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/js/all.js")
+                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/js/all.min.js")
                 .SetCdnIntegrity("sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN", "sha384-Z4FE6Znluj29FL1tQBLSSjn1Pr72aJzuelbmGmqSAFTeFd42hQ4WHTc0JC30kbQi")
                 .SetVersion("5.13.0");
         }
