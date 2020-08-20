a1e85fc6b133ded84a9db1dd39317624ac66f1be
senshi111 <banenicovic@gmail.com>
2020-08-20 17:17:04 +0200

    update font awesome

--- OrchardSkills.OrchardCore.MaterialDesignTheme/ResourceManifest.cs
+++ OrchardSkills.OrchardCore.MaterialDesignTheme/ResourceManifest.cs
@@ -22,16 +22,15 @@ namespace OrchardCore.Themes.MaterialDesignTheme
 
             manifest
                 .DefineStyle("MaterialDesignTheme-vendor-font-awesome")
-                .SetUrl("~/MaterialDesignTheme/vendor/fontawesome-free/css/all.min.css")
-                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/css/all.min.css")
-                .SetCdnIntegrity("sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V", "sha384-I4s88/QBf6OKVFMRRyjRY9wYdRoEO/PnNLQ1xY+G0OQfntKp8FxRsOg9qjmtbnvL")
-                .SetVersion("5.13.0");
+                .SetUrl("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.14.0/css/all.min.css")
+                .SetCdnIntegrity("sha256-FMvZuGapsJLjouA6k7Eo2lusoAX9i0ShlWFG6qt7SLc=")
+                .SetVersion("5.14.0");
 
             manifest
                 .DefineScript("MaterialDesignTheme-vendor-font-awesome")
-                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.0/js/all.min.js")
-                .SetCdnIntegrity("sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN", "sha384-Z4FE6Znluj29FL1tQBLSSjn1Pr72aJzuelbmGmqSAFTeFd42hQ4WHTc0JC30kbQi")
-                .SetVersion("5.13.0");
+                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.14.0/js/fontawesome.min.js")
+                .SetCdnIntegrity("sha256-dNZKI9qQEpJG03MLdR2Rg9Dva1o+50fN3zmlDP+3I+Y=")
+                .SetVersion("5.14.0");
         }
     }
 }
