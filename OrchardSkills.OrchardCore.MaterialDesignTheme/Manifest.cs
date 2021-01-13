9a6259ae0a7ba07abe177d054432cdc056373e0c
sales <sales@orchardskills.com>
2021-01-13 10:18:56 -0700

    Updated content copyright 2021

--- OrchardSkills.OrchardCore.MaterialDesignTheme/Manifest.cs
+++ OrchardSkills.OrchardCore.MaterialDesignTheme/Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Material Design Theme",
     Author = "Orchards Skills, Inc.",
     Website = "https://OrchardSkills.com",
-    Version = "1.0.0",
+    Version = "1.0.0-rc2",
     Description = "Material Design Theme for Orchard Core",
     Tags = new[] { "Material Design", "Theme", "Liquid", "Razor"}
 )]
